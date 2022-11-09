45d2fa0e57982cf4e392a9a5d804d005218f67c8
Polina Drokina <sukhova2016@inbox.ru>
2022-11-09 20:58:59 +0400

    Добавлено решение первой задачи

---
+++ task1/Program.cs
@@ -0,0 +1,61 @@
+﻿Console.WriteLine("Введите количество строк массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[n, m];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine();
+Console.WriteLine("Массив до сортировки:");
+PrintArray(numbers);
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+    {
+        for (int k = 0; k < numbers.GetLength(1) - 1; k++)
+        {
+            if (numbers[i, k] < numbers[i, k + 1]) 
+            {
+                int temp = 0;
+                temp = numbers[i, k];
+                numbers[i, k] = numbers[i, k + 1];
+                numbers[i, k + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Отсортированный массив:");
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+
+
+
+
+
+
